6fd9b34d61630003176117e6cd3f00042e3f0073
Pawan8109 <pawangupta883327@gmail.com>
2023-10-20 23:03:13 +0530

    adding frontend code for grocery-express

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "./Banner.scss";
+
+const Banner = () => {
+    return (
+        <div className="hero-banner">
+            <div className="content">
+                <div className="text-content">
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
